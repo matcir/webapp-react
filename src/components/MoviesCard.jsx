1a8bb68bdd520d5e1f5c3e9d87fc4c28d16e4fed
matcir <matteocirilloboolean@gmail.com>
2025-08-04 10:22:59 +0200

    removed movie details such as director, release year and plot from main page, removed text-underline decoration caused by Link (a) tag

--- src/components/MoviesCard.jsx
+++ src/components/MoviesCard.jsx
@@ -6,17 +6,12 @@ export default function ({ movie }) {
     return (
         <>
             <div className="col">
-                <Link to={`/movies/${movie?.id}`} className="card h-100">
+                <Link to={`/movies/${movie?.id}`} className="card h-100 text-decoration-none">
                     <figure>
                         <img className="card-img-top img-fluid" src={movie.image} alt="img" />
                     </figure>
                     <div className="card-body">
                         <h4>{movie.title}</h4>
-                        <ul className="list-unstyled">
-                            <li>Director: {movie.director}</li>
-                            <li>Genre: {movie.genre}</li>
-                            <li>Release date: {movie.release_year}</li>
-                        </ul>
                         <h6>Plot</h6>
                         <p>{movie.abstract}</p>
                     </div>
